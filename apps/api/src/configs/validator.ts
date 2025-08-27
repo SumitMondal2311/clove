2c95b75e222f6007627cc71f5ef2b1d9a441a8ab
Sumit Mondal <sumitmondal2311@gmail.com>
2025-08-27 09:34:33 +0530

    feat: setup Prisma PostgreSQL and initialize auth models; set up Docker PostgreSQL for dev/test environment

--- apps/api/src/configs/validator.ts
+++ apps/api/src/configs/validator.ts
@@ -3,6 +3,8 @@ import z from "zod";
 export const envSchema = z.object({
     NODE_ENV: z.enum(["development", "test", "production"]),
     PORT: z.string().transform(Number),
+    DATABASE_URL: z.string(),
     WEB_ORIGIN: z.string(),
     API_ORIGIN: z.string(),
+    DATABASE_MAX_RETRIES: z.string().transform(Number),
 });
